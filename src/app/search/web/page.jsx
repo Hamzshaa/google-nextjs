f80362b4743b1bfa92b31cfb9e2bd2120be2a9f1
Hamzshaa <trident32000@gmail.com>
2024-02-23 09:07:36 -0800

    finishing the search page

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,18 +1,13 @@
-import Link from "next/link";
 import WebSearchResults from "@/components/WebSearchResults";
+import Link from "next/link";
 
-const WebSearchPage = async ({ searchParams }) => {
-  const startIndex = searchParams.start || 1;
-
-  // just to see the loading effect
+export default async function WebSearchPage({ searchParams }) {
+  const startIndex = searchParams.start || "1";
   await new Promise((resolve) => setTimeout(resolve, 1000));
-
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${startIndex}`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&start=${startIndex}`
   );
-
   if (!response.ok) throw new Error("Something went wrong");
-
   const data = await response.json();
   const results = data.items;
 
@@ -20,10 +15,10 @@ const WebSearchPage = async ({ searchParams }) => {
     return (
       <div className="flex flex-col justify-center items-center pt-10">
         <h1 className="text-3xl mb-4">
-          No result found for {searchParams.searchTerm}
+          No results found for {searchParams.searchTerm}
         </h1>
         <p className="text-lg">
-          Try serchin the web or images for something else{" "}
+          Try searching the web or images for something else{" "}
           <Link href="/" className="text-blue-500">
             Home
           </Link>
@@ -32,9 +27,5 @@ const WebSearchPage = async ({ searchParams }) => {
     );
   }
 
-  return (
-    <div className="">{results && <WebSearchResults results={data} />}</div>
-  );
-};
-
-export default WebSearchPage;
+  return <div>{results && <WebSearchResults results={data} />}</div>;
+}
