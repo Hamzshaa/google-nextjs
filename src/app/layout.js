391659c1b191f251754460069abb127c95eccbda
Hamzshaa <trident32000@gmail.com>
2024-02-23 08:01:43 -0800

    add image search result page

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="realtive min-h-screen">
+      <body className="relative min-h-screen">
         {children}
         <Footer />
       </body>
